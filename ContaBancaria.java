430fc4bc9ddd26aaa6f704aef0f8c57b31df5bbc
Flor <annygabrielly730@gmail.com>
2024-02-28 17:45:30 -0300

    Corrigindo bugs

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -11,38 +11,46 @@ public class ContaBancaria {
                 Tipo de conta: Corrente
                 Saldo inicial: R$2200,00.
                 **************************
-
-
-                Operações
-
-                1- Consultar Saldo
-                2- Depositar Valor
-                3- Transferir Valor
-                4- Sair
                 """);
 
+        String menu = """
+                
+        Operações
+
+        1- Consultar Saldo
+        2- Depositar Valor
+        3- Transferir Valor
+        4- Sair
+                """;
+                
         Scanner scanner = new Scanner(System.in);
-        int operacoes = scanner.nextInt();
+        int operacoes = 0;
         double saldo = 2200.0;
         double deposita = 0;
         double valorTransferencia = 0;
 
         while (operacoes != 4) {
+            System.out.println(menu);
+            operacoes = scanner.nextInt();
+            
             if (operacoes == 1) {
                 System.out.println(saldo);
-                break;
+
             }else if (operacoes == 2) {
+                
                 System.out.println("Digite o valor que deseja depositar: ");
                 deposita = scanner.nextDouble();
                 saldo += deposita; 
                 System.out.println("Dinheiro depositado, seu novo saldo é R$" + saldo);
-                break; 
+
             }else if (operacoes == 3) {
+                
                 System.out.println("Digite o valor que deseja transferir");
+                valorTransferencia = scanner.nextDouble();
+
                 if (valorTransferencia > saldo) {
-                    System.out.println("Não é possível realizar a transferencia, pois você não possui saldo suficiente, seu saldo é " + saldo);
+                    System.out.println("Não é possível realizar a transferencia, saldo insuficiente, seu saldo atual é R$" + saldo);
                 } else{
-                    valorTransferencia = scanner.nextDouble();
                     saldo -= valorTransferencia; 
                     System.out.println("Transferencia realizada no valor de R$" + valorTransferencia + "Seu novo saldo é R$" + saldo);
                 }
